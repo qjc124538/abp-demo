2a6b3650403dbe40c3ed9595e35b402295980bc0
unknown <qjc124538@163.com>
2025-08-23 14:37:10 +0800

    初始版本

--- Qjc.AbpDemo.Application/Test/TestService.cs
+++ Qjc.AbpDemo.Application/Test/TestService.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Volo.Abp;
 using Volo.Abp.Application.Services;
 using Volo.Abp.Domain.Repositories;
+using Volo.Abp.Security.Claims;
 using Volo.Abp.Uow;
+using Volo.Abp.Users;
 
 namespace Qjc.AbpDemo
 {
     public class TestService: ApplicationService, ITestService
     {
+        private readonly ICurrentUser CurrentUser;
+
         private readonly IRepository<Test, Guid> TestRepository;
 
-        public TestService(IRepository<Test, Guid> testRepository)
+        public TestService(IRepository<Test, Guid> testRepository, ICurrentUser currentUser)
         {
             TestRepository = testRepository;
+            CurrentUser = currentUser;
         }
 
         public async Task<string> GetList()
@@ -33,6 +39,11 @@ namespace Qjc.AbpDemo
                 index++;
                 await TestRepository.InsertAsync(new Test { Name = index.ToString() }, autoSave: true);
             }
+        }
+
+        public async Task<string> GetCurrentUser()
+        {
+            return await Task.FromResult(JsonSerializer.Serialize(CurrentUser));
         }
     }
 }
